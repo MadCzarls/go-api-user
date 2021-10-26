e5f1620518609141c3fc06acd1ed1e0a9b2c67e4
Karol Kasiarz <k.kasiarz@andersenlab.com>
2021-10-26 11:04:21 +0200

    Gracefully shutdown server

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
-import "github.com/mad-czarls/go-api-user/router"
+import (
+	"github.com/mad-czarls/go-api-user/server"
+)
 
 func main() {
-	//db := database.Client()
-	//
-	////@TODO example usage below
-	//ctx := context.Background()
-	//
-	//err := db.Set(ctx, "test_key", "test_value2222", 0).Err()
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//val, err := db.Get(ctx, "test_key").Result()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println("test_key value is: ", val)
-
-	r := router.SetupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	server.Run()
 }
---
+++ server/server.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"context"
+	"github.com/mad-czarls/go-api-user/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func Run()  {
+	//setting up the server
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: router.SetupRouter(),
+	}
+
+	//starting server; it is done in goroutine since we want code below (shutdown handling) to be executed
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", server.Addr)
+
+	//create a channel to listen to interrupt signals from OS (e.g. SIGINT = ctrl+c on Linux)
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	//listening on the channel - blocks further (below code's) execution until signal won't be passed to channel
+	<-quit
+
+	//HANDLING SHUTDOWN BELOW
+	//context is for notifying the server that it has 5 seconds to finish tasks
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	//cleanup called AFTER shutdown below will be executed
+	defer func() {
+		// add custom processes cleanup here: close database, handle queues etc.
+		cancel() //canceling all process
+	}()
+
+	log.Println("Shutting down server...")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown with error: %v\n", err)
+	}
+	log.Println("Server has been shut down")
+}
